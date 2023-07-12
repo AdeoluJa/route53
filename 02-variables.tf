2836a64c1a48e6c4b78c81438431cc2feedc161f
Adeoluwa Adeyemo <108673398+AdeoluJa@users.noreply.github.com>
2023-07-12 16:51:47 +0100

    Add files via upload

---
+++ 02-variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "domain_name" {
+  description = "Domain name"
+  default     = "adeoluja.live"
+  type        = string
+}
